27e3998d37153eb66223392ae38194bf9b9720ee
Meenakshi <meenakshi@wisewaytec.com>
2024-05-21 17:57:09 -0700

    added images

--- shoppy/src/app/components/home-cards/home-cards.component.ts
+++ shoppy/src/app/components/home-cards/home-cards.component.ts
@@ -9,7 +9,7 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomeCardsComponent  implements OnInit {
   cardList = [
-    'modern_men.png', 'modern_women.png', 'traditional_men.png'
+    'modern_women.png', 'traditional_men.png'
   ];
   constructor() { }
 
